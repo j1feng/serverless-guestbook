01e653fc7ba4f46ff1d20a34833d842e1d39c598
j1feng <51950358+j1feng@users.noreply.github.com>
2021-12-28 02:05:03 -0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://fd2a29bf.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
